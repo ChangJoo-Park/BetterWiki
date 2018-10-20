6e97ae931f69898ef1e7ae4e2e4c1ade96be80eb
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-20 22:44:19 +0900

    Create article when really changed

--- src/controller/articleUpdateAction.ts
+++ src/controller/articleUpdateAction.ts
@@ -11,21 +11,27 @@ export async function articleUpdateAction(request: Request, response: Response) 
     select: ["id", "body", "title"],
   });
 
-  // FIXME: 아래의 Archive를 만드는 내용은 여기서 하지 말아야함. Subscriber 오류 수정 후 변경할 것
-  // issue : https://github.com/typeorm/typeorm/issues/2809
-  const newArchive = Archive.create({
-    article: oldArticle,
-    body: oldArticle.body,
-    title: oldArticle.title,
-    whatChanged: "이 내용은.. 자동으로 만들어줘야함",
-  });
-
   const updated = await Article.update(id, {
     body,
     title,
   });
 
-  await newArchive.save();
+  // FIXME: 아래의 Archive를 만드는 내용은 여기서 하지 말아야함. Subscriber 오류 수정 후 변경할 것
+  // issue : https://github.com/typeorm/typeorm/issues/2809
+  if (oldArticle.title !== title || oldArticle.body !== body) {
+    let message = "";
+    message += oldArticle.title !== title ? "- Title changed\n" : "";
+    message += oldArticle.body !== body ? "- Body changed\n" : "";
+
+    await Archive
+      .create({
+        article: oldArticle,
+        body: oldArticle.body,
+        title: oldArticle.title,
+        whatChanged: message,
+      })
+      .save();
+  }
 
   const updatedArticle = await Article.findOneWithDetails(id);
 
