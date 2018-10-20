c3eecb95c59e0c7a2d2c708f1dd59743194c34ae
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-20 15:55:39 +0900

    Remove comments

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,10 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { AppRoutes } from "./routes";
 
-// create connection with database
-// note that it's not active database connection
-// TypeORM creates connection pools and uses them for your requests
 createConnection().then(async (connection) => {
-
-    // create express app
     const app = express();
     app.use(bodyParser.json());
 
-    // register all application routes
     AppRoutes.forEach((route) => {
         app[route.method](route.path, (request: Request, response: Response, next: NextFunction) => {
             route.action(request, response)
@@ -24,6 +18,5 @@ createConnection().then(async (connection) => {
         });
     });
 
-    // run app
     app.listen(3000);
 });
