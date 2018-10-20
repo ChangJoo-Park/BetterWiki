5a17c1d0fedba062d9a038b28dfe1d829a89154b
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-20 22:54:01 +0900

    Remove useless codes

--- src/subscriber/ArticleSubscriber.ts
+++ src/subscriber/ArticleSubscriber.ts
@@ -6,37 +6,5 @@ import { Article } from "../entity/Article";
 export class ArticleSubscriber implements EntitySubscriberInterface<any> {
 
   public async  afterUpdate(event: UpdateEvent<Article>) {
-    try {
-      console.log(event);
-      // const titleChanged = updated.title !== origin.title;
-      // const bodyChanged = updated.body !== origin.body;
-      // const isUpdated = titleChanged || bodyChanged;
-
-      // if (isUpdated) {
-      //   let message: string = "";
-
-      //   if (titleChanged) {
-      //     message += "- title changed.\n";
-      //   }
-
-      //   if (bodyChanged) {
-      //     message += "- body changed.\n";
-      //   }
-
-      //   const newArchive = Archive.create({
-      //     article: updated,
-      //     body: updated.body,
-      //     title: updated.title,
-      //     whatChanged: message,
-      //   });
-
-      //   await newArchive.save();
-
-      //   console.log("new archive created");
-      // }
-
-    } catch (error) {
-      console.log(error);
-    }
   }
 }
