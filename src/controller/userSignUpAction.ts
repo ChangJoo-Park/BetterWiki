78786d0a5bd1b38f531a442c8699cdee3a3ac62a
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-21 19:51:39 +0900

    Add username for signup

--- src/controller/userSignUpAction.ts
+++ src/controller/userSignUpAction.ts
@@ -2,10 +2,11 @@ import { Request, Response } from "express";
 import { User } from "../entity/User";
 
 export async function userSignUpAction(request: Request, response: Response) {
-  const { email, password } = request.body;
-  const user = User.create({
+  const { email, password, username = "" } = request.body;
+  const user: User = User.create({
     email,
     passwordDigest: password,
+    username,
   });
   const savedUser = await user.save();
 
