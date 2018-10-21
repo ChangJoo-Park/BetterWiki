d264016f9492653c3a7b08d8b24ce55b5b672b30
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-21 19:48:32 +0900

    Addcolumns for user's metadata

---
+++ src/migration/1540118050131-AddMetaToUser.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table, TableColumn } from "typeorm";
+import { UserRole } from "../enum/UserRole";
+
+export class AddMetaToUser1540118050131 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        return queryRunner.addColumns("user", [
+            new TableColumn({
+                name: "username",
+                type: "VARCHAR",
+            }),
+            new TableColumn({
+                name: "about",
+                type: "VARCHAR",
+            }),
+            new TableColumn({
+                default: "''",
+                name: "avatar",
+                type: "VARCHAR",
+            }),
+            new TableColumn({
+                default: "'n/a'",
+                name: "role",
+                type: "VARCHAR",
+            }),
+        ]);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        return queryRunner.dropColumns("user", [
+            new TableColumn({
+                name: "username",
+                type: "VARCHAR",
+            }),
+            new TableColumn({
+                name: "about",
+                type: "VARCHAR",
+            }),
+            new TableColumn({
+                default: "''",
+                name: "avatar",
+                type: "VARCHAR",
+            }),
+            new TableColumn({
+                default: "n/a",
+                name: "role",
+                type: "VARCHAR",
+            }),
+        ]);
+    }
+
+}
