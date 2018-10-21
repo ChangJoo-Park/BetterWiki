eaadc17a869068d0911317929045dd3b231fe291
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-21 19:08:45 +0900

    Add comment for setup

--- src/controller/setupAction.ts
+++ src/controller/setupAction.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from "express";
 
+/**
+ * Setup 조건
+ * User가 한명도 없어야함.
+ * @param request
+ * @param response
+ */
 export async function setupAction(request: Request, response: Response) {
   return response.json([]);
 }
