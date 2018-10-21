7533dc1475bf8c7ca4bb0991e16f2a709f428c2c
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-21 23:16:08 +0900

    return reloaded article when article created

--- src/controller/articleCreateAction.ts
+++ src/controller/articleCreateAction.ts
@@ -2,13 +2,17 @@ import { Request, Response } from "express";
 import { Article } from "../entity/Article";
 
 export async function articleCreateAction(request: Request, response: Response) {
+
   const { title, body, topicId } = request.body;
 
   const newArticle = new Article();
   newArticle.title = title;
   newArticle.body = body;
   newArticle.topic = topicId;
+  newArticle.user = request.user;
 
   const savedArticle = await newArticle.save();
-  response.send(savedArticle);
+
+  const reloadArticle = await Article.findOneWithDetails(savedArticle.id);
+  response.send(reloadArticle);
 }
