9b8843f6a4ed28620278a03bc9c9b93c4bd12db8
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-21 22:52:13 +0900

    Using only admin middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import * as bodyParser from "body-parser";
-import { Request, Response } from "express";
 import * as express from "express";
+import { Request, Response } from "express";
 import { NextFunction } from "express-serve-static-core";
 import * as logger from "morgan";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
+import { onlyAdmin } from "./authentication/onlyAdmin";
 import "./authentication/passport";
 import { passport } from "./authentication/passport";
 import { AppRoutes } from "./routes";
@@ -16,13 +17,24 @@ createConnection().then(async (connection) => {
 
     AppRoutes.forEach((route) => {
         if (route.needAuth) {
-            app[route.method](
-                route.path,
-                passport.authenticate("jwt", { session: false }),
-                (request: Request, response: Response, next: NextFunction) => {
-                    return route.action(request, response).catch((error) => next(error));
-                },
-            );
+            if (route.onlyAdmin) {
+                app[route.method](
+                    route.path,
+                    passport.authenticate("jwt", { session: false }),
+                    onlyAdmin,
+                    (request: Request, response: Response, next: NextFunction) => {
+                        return route.action(request, response).catch((error) => next(error)); ,
+                    },
+                );
+            } else {
+                app[route.method](
+                    route.path,
+                    passport.authenticate("jwt", { session: false }),
+                    (request: Request, response: Response, next: NextFunction) => {
+                        return route.action(request, response).catch((error) => next(error));
+                    },
+                );
+            }
         } else {
             app[route.method](
                 route.path,
