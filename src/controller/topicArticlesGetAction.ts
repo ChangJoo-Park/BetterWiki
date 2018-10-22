a802bf4e06885219edeb24b5d1824f302ecaa968
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-22 22:07:18 +0900

    Update topic articles get actions. using relations

--- src/controller/topicArticlesGetAction.ts
+++ src/controller/topicArticlesGetAction.ts
@@ -3,12 +3,7 @@ import { Topic } from "../entity/Topic";
 
 export async function topicArticlesGetAction(request: Request, response: Response) {
   const topic = await Topic.findOne(request.params.id, {
-    join: {
-      alias: "topic",
-      leftJoinAndSelect: {
-        articles: "topic.articles",
-      },
-    },
+    relations: ["articles"],
   });
 
   response.send(topic);
