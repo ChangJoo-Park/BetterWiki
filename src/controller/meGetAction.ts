87cb453655bb879fd82f69d5746895637f1e5ec5
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-22 21:37:30 +0900

    implements me get actions

--- src/controller/meGetAction.ts
+++ src/controller/meGetAction.ts
@@ -1,5 +1,23 @@
 import { Request, Response } from "express";
+import { FindManyOptions, FindOneOptions } from "typeorm";
+import { Archive } from "../entity/Archive";
+import { Article } from "../entity/Article";
 
 export async function meGetAction(request: Request, response: Response) {
-  return response.send(request.user);
+  const where: FindManyOptions = {
+    loadRelationIds: true,
+    where: {
+      user: request.user,
+    },
+  };
+
+  const archives = Archive.find(where);
+  const articles = Article.find(where);
+  const results = await Promise.all([archives, articles]);
+
+  return response.send({
+    archives: results[0],
+    articles: results[1],
+    user: request.user,
+  });
 }
