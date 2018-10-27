bd74daed20b2be4d9bab4c59c9a1870d7e8268e7
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-27 13:22:57 +0900

    Need setup if database has not service or admin

--- src/controller/setupCheckAction.ts
+++ src/controller/setupCheckAction.ts
@@ -1,18 +1,14 @@
 import { Request, Response } from "express";
+import { Service } from "../entity/Service";
 import { User } from "../entity/User";
+import { UserRole } from "../enum/UserRole";
 
 export async function setupCheckAction(request: Request, response: Response) {
-  const users = await User.findOne();
-  let message = "";
-  let needSetup = false;
+  const admin = await User.findOne({ role: UserRole.Admin });
+  const service = await Service.findOneWithDetails();
 
-  if (users) {
-    message = "You don't need any setup";
-    needSetup = false;
-  } else {
-    message = "You need setup for use";
-    needSetup = true;
-  }
+  const needSetup = !admin || !service;
+  const message = needSetup ? "You need setup to use" : "You don't need any setup";
 
   return response.json({
     message,
