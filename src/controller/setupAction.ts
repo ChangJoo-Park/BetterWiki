91927c2b965c02a086abf117847ca94f1ed5f6a5
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-27 22:10:19 +0900

    Create new topic when first setup

--- src/controller/setupAction.ts
+++ src/controller/setupAction.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Service } from "../entity/Service";
+import { Topic } from "../entity/Topic";
 import { User } from "../entity/User";
 import { UserRole } from "../enum/UserRole";
 
@@ -10,9 +11,9 @@ import { UserRole } from "../enum/UserRole";
  * @param response
  */
 export async function setupAction(request: Request, response: Response) {
-  const { admin, service } = request.body;
+  const { admin: requestedAdmin, service: requestedService } = request.body;
 
-  const { email, password, username } = admin;
+  const { email, password, username } = requestedAdmin;
   const newUser = User.create({
     email,
     passwordDigest: password,
@@ -20,19 +21,23 @@ export async function setupAction(request: Request, response: Response) {
     username,
   });
 
-  const savedUser = await newUser.save();
-
-  const { name, description } = service;
+  const { name, description } = requestedService;
 
   const newService = Service.create({
     description,
     name,
   });
 
-  const savedService = await newService.save();
+  const newTopic = Topic.create({
+    description: "knowledge for everyone",
+    name: "general",
+  });
+
+  const [admin, service, topic] = await Promise.all([newUser.save(), newService.save(), newTopic.save()]);
 
   return response.json({
-    admin: savedUser,
-    service: savedService,
+    admin,
+    service,
+    topic,
   });
 }
