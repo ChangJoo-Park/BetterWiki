693df730d73c10d8c3ea1c35f8cba9c707b73918
ChangJoo Park <pcjpcj2@gmail.com>
2018-11-03 11:55:12 +0900

    Add topic scenario

--- src/controller/articleCreateAction.ts
+++ src/controller/articleCreateAction.ts
@@ -1,18 +1,28 @@
 import { Request, Response } from "express";
 import { Article } from "../entity/Article";
+import { Topic } from "../entity/Topic";
 
 export async function articleCreateAction(request: Request, response: Response) {
 
-  const { title, body, topicId } = request.body;
+    const { title, body, topic } = request.body;
 
-  const newArticle = new Article();
-  newArticle.title = title;
-  newArticle.body = body;
-  newArticle.topic = topicId;
-  newArticle.user = request.user;
+    const newArticle = new Article();
+    newArticle.title = title;
+    newArticle.body = body;
 
-  const savedArticle = await newArticle.save();
+    const existsTopic = await Topic.findOne({ where: { name: topic } })
+    if (existsTopic) {
+        newArticle.topic = existsTopic
+    } else {
+        const newTopic = await Topic.create({ name: topic })
+        const savedTopic = await Topic.save(newTopic)
+        newArticle.topic = savedTopic
+    }
 
-  const reloadArticle = await Article.findOneWithDetails(savedArticle.id);
-  response.send(reloadArticle);
+    newArticle.user = request.user;
+
+    const savedArticle = await newArticle.save();
+
+    const reloadArticle = await Article.findOneWithDetails(savedArticle.id);
+    response.send(reloadArticle);
 }
